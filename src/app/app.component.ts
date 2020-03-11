5f173c0c0f9daad6881f7ea42d51ad4ffac6b505
Will <doggy.huang@gmail.com>
2020-03-11 13:54:12 +0800

    任務03：使用內嵌繫結顯示資料

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'skb200311';
+  title = 'conduit';
+  subtitle = 'A place to share your knowledge.';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { TagsComponent } from './tags/tags.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    ArticlesComponent,
+    TagsComponent
   ],
   imports: [
     BrowserModule
